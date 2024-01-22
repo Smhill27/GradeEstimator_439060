c51f74770d08aa4a0aa85b7472b6c98974303dbc
Smhill27 <156019908+Smhill27@users.noreply.github.com>
2024-01-21 18:33:47 -0600

    Update GradeEstimator_463467.py

--- GradeEstimator_463467.py
+++ GradeEstimator_463467.py
@@ -1,9 +1,4 @@
 first_name = input("Enter your first name:")
-last_name = input("Enter your last name:")
-print (x)
-print (y)
-print (z)
-first_name = input("Enter your first name:")
 #This captures the entry for first name.
 last_name = input("Enter your last name:")
 #This captures the entry for last name.
@@ -12,3 +7,4 @@ full_name = first_name + " " + last_name
 print(full_name)
 #This will show the full name for both variables.
 first_name = str()
+#Defines the entry for first name.
